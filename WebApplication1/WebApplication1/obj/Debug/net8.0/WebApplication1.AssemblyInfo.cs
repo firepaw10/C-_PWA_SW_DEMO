c6f964bf8f54d591b244d99c1eb289a5642b801f
fonsecag <grantfonseca@gmail.com>
2024-08-12 09:01:56 -0700

    Fixed

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c50031e1a1ba7146ed625afe6dbaaf34edc7980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51d6bdda8dd0f0c917632f496811d3f411442e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
